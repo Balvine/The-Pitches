b67a07bfc6bbc91dcf971ca67e5d5c000e22730f
Balvine <balvy075@gmail.com>
2020-09-21 23:02:54 +0300

    added configurations

--- config.py
+++ config.py
@@ -6,3 +6,46 @@ class Config:
     '''
     SECRET_KEY = 'barl'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://balvine:balito20@localhost/pitch'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
