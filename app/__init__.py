1fd8f80bdff9693479cf6f44a41a92e85f5ac0b2
Balvine <balvy075@gmail.com>
2020-09-22 09:17:18 +0300

    added bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 from config import config_options 
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_sqlalchemy import SQLAlchemy
