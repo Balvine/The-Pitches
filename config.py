5370258779a75d7e868627edb661414596e74daf
Balvine <balvy075@gmail.com>
2020-09-23 15:28:27 +0300

    created user

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = 'barl'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://balvine:balito20@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://balvine:balito20@localhost/pitches'
     
     
     
--- manage.py
+++ manage.py
@@ -1,25 +1,30 @@
-from app import create_app,db
-# from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
-from app.models import User
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Comment, Post
+from flask_migrate import Migrate, MigrateCommand
 
 
+# Creating app instance
+app = create_app('development')
 
-#creating an app instance  
-app = create_app('development')  
-# configure_uploads(app, (csvfiles,), lambda app: '/var/uploads')
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
-# manager = Manager(app)
-# manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
-# manager.add_command('server',Server)
-
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Role = Role )
+    return dict(app=app, db=db, User=User, Comment=Comment, Post=Post)
+
+
 if __name__ == '__main__':
-    manager.ru
+    manager.run()
