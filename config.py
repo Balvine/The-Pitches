5a580372a11e13aeb82fda5959a2a0dfa138e153
Balvine <balvy075@gmail.com>
2020-09-24 18:11:31 +0300

    added migrations

--- config.py
+++ config.py
@@ -1,51 +1,76 @@
+
 import os
 
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    SECRET_KEY = 'barl'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://balvine:balito20@localhost/pitches'
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-    
-    
+        """
+        General configuration parent class
+        """
+        SECRET_KEY = 'barl'
+        SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitches'
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guesss'
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        UPLOADED_PHOTOS_DEST = 'app/static/photos'
+        MAIL_SERVER = 'smtp.googlemail.com'
+        MAIL_PORT = 587
+        MAIL_USE_TLS = True
+        MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+        
+        MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+        print(MAIL_PASSWORD)
 
-#     DEBUG = True
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitches'
+class ProdConfig(Config):
+    """
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with General
+        configuration settings
+    """
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+    Config: The parent configuration class with General
+    configuration settings
+    """
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitches'
+    # SECRET_KEY = os.urandom(38)
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+} 
+    
+# import os
+
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     SECRET_KEY = 'barl'
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://balvine:balito20@localhost/pitches'
+    
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
